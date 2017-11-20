fad3778d00e61dacf86f871e567e00790c3a2b90
zongzhec <zongzhe_chen@sina.com>
2017-11-20 21:43:56 +0800

    MySQL connection
    add sample connection to MySQL.

---
+++ sql/query.sql
@@ -0,0 +1,5 @@
+insert into index_profit_prod.sh_510500 values ('2017-11-20', 0.71, 0.82, 'Initialize');
+
+select * from index_profit_prod.sh_510500
+
+`510500``510500`
--- src/foo/zongzhe/indexprofit/model/StartingPoint.java
+++ src/foo/zongzhe/indexprofit/model/StartingPoint.java
@@ -1,14 +1,78 @@
 package foo.zongzhe.indexprofit.model;
 
 import foo.zongzhe.utils.LogUtil;
+import java.sql.*;
 
 public class StartingPoint {
-	
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/index_profit_prod?autoReconnect=true&useSSL=false";
+	static final String USER = "root";
+	static final String PASS = "root";
+
 	static LogUtil log = new LogUtil();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		initialize();
+		getSampleData();
+	}
+
+	private static void getSampleData() {
+		// TODO Auto-generated method stub
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			// 注册 JDBC 驱动
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// 打开链接
+			System.out.println("连接数据库...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// 执行查询
+			System.out.println(" 实例化Statement对...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT * FROM sh_510500";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// 展开结果集数据库
+			while (rs.next()) {
+				// 通过字段检索
+				String date = rs.getString("date");
+				double price_origin = rs.getDouble("price_origin");
+				double price_balanced = rs.getDouble("price_balanced");
+				String event = rs.getString("event");
+
+				// 输出数据
+				System.out.println("date: " + date);
+			}
+			// 完成后关闭
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// 处理 JDBC 错误
+			se.printStackTrace();
+		} catch (Exception e) {
+			// 处理 Class.forName 错误
+			e.printStackTrace();
+		} finally {
+			// 关闭资源
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			} // 什么都不做
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		System.out.println("Goodbye!");
 	}
 
 	private static void initialize() {
